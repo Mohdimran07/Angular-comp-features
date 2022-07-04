383cdf4e9d7a2a009eca66a91eaf3892cbd9c9b9
Mohammed Imran <mohdimran97057@gmail.com>
2022-07-04 15:44:47 +0530

    new Component for list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailsComponent } from './components/hero-details/hero-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailsComponent,
   ],
   imports: [
     BrowserModule,
